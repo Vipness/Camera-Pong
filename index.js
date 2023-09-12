a5195593904b79a70da31796ef0fcce6dc28a02b
Vipness <vid.kovacic2005@gmail.com>
2023-09-12 20:06:54 +0200

    Track a color

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let canvas, ctx, video;
+const color = [181, 12, 85];
+
 function main() {
     canvas = document.querySelector("#camera");
     navigator.mediaDevices.getUserMedia({ video: true })
@@ -17,5 +19,46 @@ function animate() {
     canvas.height = video.videoHeight;
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
+    const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const locations = getLocationsWithColor(imgData, { r: 255, g: 0, b: 0 });
+
+    ctx.fillStyle = "yellow";
+    locations.forEach(loc => {
+        ctx.fillRect(loc.x, loc.y, 1, 1);
+    })
     requestAnimationFrame(animate);
+}
+
+function getLocationsWithColor(imgData, color) {
+    const locations = [];
+
+    for (let i = 0; i < imgData.data.length; i += 4) {
+        const pxColor = {
+            r: imgData.data[i],
+            g: imgData.data[i + 1],
+            b: imgData.data[i + 2]
+        };
+
+        const pxIndex = i / 4;
+
+        const pxLocation = {
+            x: pxIndex % imgData.width,
+            y: Math.floor(pxIndex / imgData.width)
+        }
+
+        if (colorsMatch(pxColor, color)) {
+            locations.push(pxLocation);
+        }
+    }
+    return locations;
+}
+
+function colorsMatch(pxColor, color, threshold = 160) {
+    return sqDistance(pxColor, color) < threshold ** 2;
+}
+
+function sqDistance(pxColor, color) {
+    return (pxColor.r - color.r) ** 2 +
+        (pxColor.g - color.g) ** 2 +
+        (pxColor.b - color.b) ** 2
 }
