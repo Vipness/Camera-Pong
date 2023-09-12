20b695cf6aec2ef04cd93c59e684b749da2b656a
Vipness <vid.kovacic2005@gmail.com>
2023-09-12 16:57:00 +0200

    Animate the canvas

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const myVideo = document.querySelector("#myVideo");
-
+let canvas, ctx, video;
 function main() {
+    canvas = document.querySelector("#camera");
     navigator.mediaDevices.getUserMedia({ video: true })
         .then(function (rawData) {
-            myVideo.srcObject = rawData;
-            myVideo.play();
+            video = document.createElement("video");
+            video.srcObject = rawData;
+            video.play();
+            video.onloadeddata = animate;
         })
         .catch(function (error) { alert(error) })
+}
+
+function animate() {
+    const ctx = canvas.getContext("2d");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    requestAnimationFrame(animate);
 }
