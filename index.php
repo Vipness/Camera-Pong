18b3ed0bc5bcf7c38f810b2d25c1f99f2df1e864
Vipness <vid.kovacic2005@gmail.com>
2024-04-03 20:43:15 +0200

    Change menu design

--- index.php
+++ index.php
@@ -11,30 +11,51 @@
 </head>
 
 <body>
-    <div class="wrapper">
-        <h1>Camera Pong</h1>
+    <div class="page">
+        <!-- <h1>Camera Pong</h1> -->
+        
+        <div class="menu">
+            <div class="buttons">
+                    <button class="btnPlay"><a href="#settings">Play</a></button>
+                    <button class="btnInactive"><a href="./subpages/calibrator.html">Calibrate Color</a></button>
+            </div>
 
-        <label for="rounds">Number of rounds: <span id="roundsText"></span></label>
-        <input type="range" min="2" max="6" value="3" id="rounds">
-
-        <label for="speed">Speed: <span id="speedText"></span></label>
-        <input type="range" min="2" max="8" value="4" id="speed">
-
-        <div class="buttons">
-            <button class="btnStart"><a href="./subpages/pong.html">Start Game</a></button>
-            <button class="btnCalibrate"><a href="./subpages/calibrator.html">Calibrate Color</a></button>
+            <?php
+                session_start();
+                if(!isset($_SESSION['username'])){
+                    echo "<p class='login'>Want to save your score? <a href='./subpages/login.php'>Login</a></p>";
+                }
+                else{
+                    $user = $_SESSION['username'];
+                    echo "<p class='userWelcome'>Welcome, $user! <a href='./php/logout.php' class='logout'>Logout</a></p>";
+                }
+            ?>
         </div>
+    </div>
 
-        <?php
-            session_start();
-            if(!isset($_SESSION['username'])){
-                echo "<p class='login'>Want to save your score? <a href='./subpages/login.php'>Login</a></p>";
-            }
-            else{
-                $user = $_SESSION['username'];
-                echo "<p class='userWelcome'>Welcome, $user! <a href='./php/logout.php' class='logout'>Logout</a></p>";
-            }
-        ?>
+    <div class="page">
+        <div class="settings-wrapper" id="settings">
+            <h2>Settings</h2>
+
+            <div class="settings">
+                <label for="rounds">Number of rounds</label>
+                <div class="setting">
+                    <input type="range" min="2" max="6" value="3" id="rounds">
+                    <span id="roundsText"></span>
+                </div>
+
+                <label for="speed">Difficulty</label>
+                <div class="setting">
+                    <input type="range" min="2" max="8" value="4" id="speed">
+                    <span id="speedText"></span>
+                </div>
+            </div>
+
+            <div class="settings-buttons">
+                <button class="btnInactive"><a href="#">Back</a></button>
+                <button class="btnPlay"><a href="./subpages/pong.html">Start Game</a></button>
+            </div>  
+        </div>
     </div>
 </body>
 
